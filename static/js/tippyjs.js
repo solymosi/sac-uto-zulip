26ed15156cfc298d729001fe4d89075c5339c81a
Aman Agrawal <amanagr@zulip.com>
2021-12-10 08:07:42 +0000

    compose: Change UI which toggles `enter_sends` setting.
    Use a popover which displays both the options instead of long text.
    We only use a small text indicating the current state which user
    can click on to trigger the popover.

--- static/js/tippyjs.js
+++ static/js/tippyjs.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 import tippy, {delegate} from "tippy.js";
 
 import * as message_lists from "./message_lists";
+import * as popover_menus from "./popover_menus";
 import * as reactions from "./reactions";
 import * as rows from "./rows";
 import * as timerender from "./timerender";
@@ -183,6 +184,18 @@ export function initialize() {
         appendTo: () => document.body,
         onHidden(instance) {
             instance.destroy();
+        },
+    });
+
+    delegate("body", {
+        target: [".enter_sends_true", ".enter_sends_false"],
+        content: "Switch hotkey",
+        onShow() {
+            // Don't show tooltip if the popover is displayed.
+            if (popover_menus.compose_enter_sends_popover_displayed) {
+                return false;
+            }
+            return true;
         },
     });
 }
